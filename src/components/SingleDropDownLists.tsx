b559cde2079959dea2c67037066949b00347e248
dbarron4102024S <davidbarron410@gmail.com>
2025-08-21 14:52:52 -0400

    Create new component to create and populate a single dropdown

---
+++ src/components/SingleDropDownLists.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import { useState } from "react"
+import Image from 'next/image'
+
+interface SingleDropdownProps {
+    label: string
+    options: string[]
+    selectedValue: string
+    onValueChange: (value: string) => void
+    placeholder?: string
+}
+
+const SingleDropdown = ({ label, options, selectedValue, onValueChange, placeholder }: SingleDropdownProps) => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleOptionClick = (option: string) => {
+        onValueChange(option)
+        setIsOpen(false)
+    }
+
+    return (
+        <div className="relative">
+            <div className="cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
+                <div className="filter-trigger">
+                    <figure>
+                        <Image src="/assets/icons/hamburger.svg" alt="menu" width={14} height={14} />
+                        {selectedValue || placeholder || label}
+                    </figure>
+                    <Image src="/assets/icons/arrow-down.svg" alt="arrow-down" width={20} height={20} />
+                </div>
+
+                {isOpen && (
+                    <ul className="dropdown">
+                        {options.map((option) => (
+                            <li 
+                                key={option} 
+                                className={`list-item ${selectedValue === option ? 'selected' : ''}`}
+                                onClick={() => handleOptionClick(option)}
+                            >
+                                {option}
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default SingleDropdown
