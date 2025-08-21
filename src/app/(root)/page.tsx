168b066a9cb7994485bb1d322b4c182afbf0e6e6
dbarron4102024S <davidbarron410@gmail.com>
2025-08-21 13:54:09 -0400

    now fetching all learning sessions using specified date

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import Header from "@/components/Header"
 import VideoCard from '@/components/VideoCard'
+import { getAllLearningSessions } from '@/lib/actions/getAllLearningSessions'
 
 import { dummyCards } from "@/constants"
 
-const Page = () => {
+const Page = async () => {
+  const allLearningSessions = await getAllLearningSessions()
+  
+  if (allLearningSessions.success) {
+    console.log('Learning Session Data:', allLearningSessions.data)
+    console.log('Number of learning sessions found:', allLearningSessions.data?.length || 0)
+  } else {
+    console.error('Failed to fetch user demographics:', allLearningSessions.error)
+  }
+
   return (
     <main className="wrapper page">
-      <Header title="All Videos" subHeader="Public Library"/>
+      <Header title="All Learning Sessions" subHeader="Public Library"/>
 
       <section className="video-grid">
         {dummyCards.map((card) => (
