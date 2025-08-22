8f410315ed91fa18a917a2dc627aec6ac586e69e
David <dbarron410@vt.edu>
2025-08-22 00:34:24 -0400

    add view profiles and generate profiles button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import DropdownList from './MultipleDropdownLists'
 
@@ -13,38 +16,58 @@ interface SharedHeaderProps {
 
 const Header = ({ subHeader, title, userImg, onFiltersChange, availableLessons }: SharedHeaderProps) => {
   return (
-    <header className="header">
-        <section className="header-container">
-            <div className="details">
-                {userImg && (
-                    <Image src={userImg} 
-                    alt="user" width={66} height={66} className="rounded-full"/>
-                )}
+    <>
+      <header className="header">
+          <section className="header-container">
+              <div className="details">
+                  {userImg && (
+                      <Image src={userImg} 
+                      alt="user" width={66} height={66} className="rounded-full"/>
+                  )}
 
-                <article>
-                    <p>
-                        {subHeader}
-                    </p>
-                    <h1>
-                        {title}
-                    </h1>
-                </article>
-            </div>
-        </section>
+                  <article>
+                      <p>
+                          {subHeader}
+                      </p>
+                      <h1>
+                          {title}
+                      </h1>
+                  </article>
+              </div>
 
-        <section className="search-filter">
-            <div className="search">
-                <input 
-                    type="text"
-                    placeholder="Search sessions, students, concepts, algorithms..."
-                />
-                <Image src="/assets/icons/search.svg" alt="search"
-                width={16} height={16} />
-            </div>
+               <aside>
+                <Link href="/upload">
+                    <Image src="/assets/icons/users.svg" 
+                    alt="view" width={16} height={16} />
+                    <span>
+                        View profiles
+                    </span>
+                </Link>
+                <div className="record">
+                    <button className="primary-btn">
+                        <Image src="/assets/icons/wand.svg" alt="generate" width={16} height={16}></Image>
+                        <span>
+                            Generate profiles
+                        </span>
+                    </button>
+                </div>
+            </aside>
+          </section>
 
-            <DropdownList onFiltersChange={onFiltersChange} availableLessons={availableLessons} />
-        </section>
-    </header>
+          <section className="search-filter">
+              <div className="search">
+                  <input 
+                      type="text"
+                      placeholder="Search sessions, students, concepts, algorithms..."
+                  />
+                  <Image src="/assets/icons/search.svg" alt="search"
+                  width={16} height={16} />
+              </div>
+
+              <DropdownList onFiltersChange={onFiltersChange} availableLessons={availableLessons} />
+          </section>
+      </header>
+    </>
   )
 }
 
