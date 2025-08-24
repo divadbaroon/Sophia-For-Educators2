da6fec078e1311c1c7359266b306f3f7ce6e26db
David <dbarron410@vt.edu>
2025-08-24 16:52:51 -0400

    remove unused import

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,6 @@
 import React from 'react'
 import Link from "next/link"
 import Image from "next/image"
-import { useRouter } from "next/navigation"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { Button } from "@/components/ui/button"
 import { signOut } from "@/app/(auth)/login/actions"
