964acba1f3375df2e138c892d31aaf6c959014d8
dbarron4102024S <davidbarron410@gmail.com>
2025-08-26 13:48:03 -0400

    move location of navbar

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react"
-import NavbarWrapper from "@/components/NavbarWrapper"
+import NavbarWrapper from "@/components/navbar/NavbarWrapper"
 
 const Layout = ({ children }: { children: ReactNode}) => {
     return (
