ecd186ddd5bec5a8508e102b3dbedd4f00bedfff
David <dbarron410@vt.edu>
2025-09-02 00:45:42 -0400

    init test results types

---
+++ src/components/testResults/types.ts
@@ -0,0 +1,11 @@
+import { AgentInfo } from "@/components/configuration/types"
+
+export interface ValidationInterfaceProps {
+  condition?: "1" | "2"
+  isRunningTests?: boolean
+  currentStep?: number
+  steps?: string[]
+  agentInfo?: AgentInfo | null
+  isSaving?: boolean
+  onUpdateConfig?: (name: string, prompt: string, firstMessage: string) => Promise<AgentInfo | null>
+}
